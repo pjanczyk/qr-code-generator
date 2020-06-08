ff58ad0e7e1e0d135d9e031e4aebb63f57c609ac
Piotr Janczyk <pjanczyk97@gmail.com>
2020-06-08 19:12:21 +0200

    Configure ECS deployment

---
+++ deploy.sh
@@ -0,0 +1,15 @@
+#!/usr/bin/env bash
+set -euo pipefail
+cd "$(dirname "$0")"
+
+echo "-> Building Docker images..."
+docker-compose build
+
+echo "-> Pushing images to Docker registry..."
+docker push pjanczyk/qr-code-generator_web
+docker push pjanczyk/qr-code-generator_qr-code-service
+
+echo "-> Running containers on ECS..."
+ecs-cli compose up
+
+echo "-> Done"
